ebd8e701faa8f0003c69c3e1c4ec99aac3403e9f
Evatitan <yinguihua2014@gmail.com>
2022-01-05 19:51:12 +0100

    build food order demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React, { useState } from 'react';
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import Header from './components/Layout/Header';
 
 function App() {
-	const [ usersList, setUsersList ] = useState([]);
-
-	const addUserHandler = (name, age) => {
-		setUsersList((prevUsersList) => {
-			console.log(name, age);
-			console.log(prevUsersList);
-			return [ ...prevUsersList, { name, age, id: Math.random() } ];
-		});
-	};
-
 	return (
-		<div>
-			<AddUser onAddUser={addUserHandler} />
-			<UsersList users={usersList} />
-		</div>
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
 	);
 }
 
