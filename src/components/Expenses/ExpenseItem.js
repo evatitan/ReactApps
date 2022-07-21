c6bf58b6fbd8c7e459c15a4386d6629b79336b7b
Evatitan <yinguihua2014@gmail.com>
2022-07-21 23:50:35 +0200

    listening to events and event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,8 +2,10 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-
 function ExpenseItem(props) {
+	const clickHandler = () => {
+		console.log('clicked');
+	};
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
@@ -12,6 +14,9 @@ function ExpenseItem(props) {
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
+			{/* here we just pass a pointer to event, not to excute function, 
+			because when the component to layout, is to early to excute clickHandler function. */}
+			<button onClick={clickHandler}>click</button>
 		</Card>
 	);
 }
